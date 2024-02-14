2e08e32dc33ca9886177e354b69838a4187ea282
Luis de la Torre <darkknightandroid@gmail.com>
2024-02-14 10:52:38 -0600

    deployd

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://deeperunmade.github.io',
   output: "server",
   adapter: netlify()
 });
