bc70ab1efce48281074e6b302b1878720a29b40a
Luis de la Torre <darkknightandroid@gmail.com>
2024-02-14 11:39:01 -0600

    Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://deeperunmade.github.io',
-  base:'/Portafolio-web-Astro',
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
